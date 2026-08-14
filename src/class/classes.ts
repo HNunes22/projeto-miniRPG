3ad5784a7760cb7aeefc8b8b8b3263f0f98aa7e0
nunes <hugoanunes2003@gmail.com>
2026-08-14 17:09:22 -0300

    Atualizacao
    - Organizando e Adicionando melhor as classes

--- src/class/classes.ts
+++ src/class/classes.ts
@@ -34,5 +34,53 @@ class Archer {
   }
 }
 
+// Tank
+
+class Tank {
+  private readonly className: string = 'Tank';
+  private status = new StatusTank();
+  private weapon = new Shield();
+
+  showInfos(): string {
+    return motorMethods.showInfos(this.status, this.weapon);
+  }
+
+  levelUp(): void {
+    motorMethods.levelup(this.status);
+  }
+}
+
+// Assassin
+
+class Assassin {
+  private readonly className: string = 'Assassin';
+  private status = new StatusAssassin();
+  private weapon = new Dagger();
+
+  showInfos(): string {
+    return motorMethods.showInfos(this.status, this.weapon);
+  }
+
+  levelUp(): void {
+    motorMethods.levelup(this.status);
+  }
+}
+
+// Warrior
+
+class Warrior {
+  private readonly className: string = 'Warrior';
+  private status = new StatusWarrior();
+  private weapon = new Sword();
+
+  showInfos(): string {
+    return motorMethods.showInfos(this.status, this.weapon);
+  }
+
+  levelUp(): void {
+    motorMethods.levelup(this.status);
+  }
+}
 // ------------------------------------------------------- //
+
 
